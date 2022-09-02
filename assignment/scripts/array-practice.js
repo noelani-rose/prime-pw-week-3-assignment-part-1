e52c72e76f3dee6733c81228237e12ec66353e38
Noelani West <noelani.rose14@gmail.com>
2022-09-01 21:23:57 -0500

    stretch goals 1, 3, & 4

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -87,16 +87,23 @@ console.log('The foods are now', foodArray);
 // 4.e (STRETCH) TODO: Replace the second food in your array
 //      with another one of your favorite foods.
 //      Then log the updated array.
+foodArray[1] = 'pho'
+console.log('the new food list is now', foodArray);
 
 // 4.f (STRETCH) TODO: Sort your favoriteFoods array
 //     in reverse alphabetical order. Log the array.
 
+
 // 4.g (STRETCH) TODO: Convert your array to a string
 //     putting the word "and" between each item.
 //     eg "tacos and pizza and pasta". Log the string.
+console.log(foodArray.join(' and '));
 
 // 4.h (STRETCH) TODO: Make a new array that combines 
 //     the favorite foods array with the animals array.
 //     Then log the new array.
 //     It should look something like:
 //     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+let newArray = foodArray.concat(animalArray);
+console.log('the combined arrays', newArray);
+
