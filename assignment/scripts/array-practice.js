faf649830826aa53bb0cad8a23d228c05c72db49
Noelani West <noelani.rose14@gmail.com>
2022-09-04 16:45:23 -0500

    finished part 1

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -24,6 +24,7 @@ console.log('Number of animals:', animalArray.length);
 // 2.a. TODO: Log to the console the number of foods in your array
 console.log('number of food items:', foodArray.length);
 
+
 // 3. Accessing array items
 console.log('--- 3. Accessing items in an array ---');
 
